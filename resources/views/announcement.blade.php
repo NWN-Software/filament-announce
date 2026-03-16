da8eb6df7fc0a8b8debb5df4b84b4d053a252f3a
Wannes Matthys <wannesmatthys@gmail.com>
2026-03-16 13:30:28 +0100

    Update blade

--- resources/views/announcement.blade.php
+++ resources/views/announcement.blade.php
@@ -8,15 +8,19 @@
     $actions = $notification->getActions();
     $color = $notification->getColor();
 
-    if (! $titleAlignment instanceof Alignment) {
-        $titleAlignment = filled($titleAlignment) ? (Alignment::tryFrom($titleAlignment) ?? $titleAlignment) : null;
+    if (!$titleAlignment instanceof Alignment) {
+        $titleAlignment = filled($titleAlignment) ? Alignment::tryFrom($titleAlignment) ?? $titleAlignment : null;
     }
 
-    if (! $bodyAlignment instanceof Alignment) {
-        $bodyAlignment = filled($bodyAlignment) ? (Alignment::tryFrom($bodyAlignment) ?? $bodyAlignment) : null;
+    if (!$bodyAlignment instanceof Alignment) {
+        $bodyAlignment = filled($bodyAlignment) ? Alignment::tryFrom($bodyAlignment) ?? $bodyAlignment : null;
     }
 
-    $colorClasses = \Illuminate\Support\Arr::toCssClasses(['flex items-center border border-transparent px-6 py-2 gap-4', 'bg-white text-gray-950 dark:bg-white/5 dark:text-white' => $color === 'gray', 'bg-custom-600 text-white dark:bg-custom-500' => $color !== 'gray']);
+    $colorClasses = \Illuminate\Support\Arr::toCssClasses([
+        'flex items-center border border-transparent px-6 py-2 gap-4',
+        'bg-white text-gray-950 dark:bg-white/5 dark:text-white' => $color === 'gray',
+        'bg-custom-600 text-white dark:bg-custom-500' => $color !== 'gray',
+    ]);
 
     $colorStyles = \Illuminate\Support\Arr::toCssStyles([
         \Filament\Support\get_color_css_variables($color, shades: [400, 500, 600]) => $color !== 'gray',
@@ -34,10 +38,8 @@
             <x-filament::icon icon="{{ $icon }}" class="h-6 w-6" />
         </div>
     @endif
-    <div @class([
-        'w-full flex-1',
-    ])>
-        @if ($title && ! $body && $actions)
+    <div @class(['w-full flex-1'])>
+        @if ($title && !$body && $actions)
             <div @class([
                 'flex flex-row flex-wrap items-center gap-4 leading-none',
                 match ($titleAlignment) {
@@ -50,9 +52,9 @@
             ])>
                 <h5 class="font-semibold">{{ $title }}</h5>
 
-                <x-filament-notifications::actions :actions="$actions" class="flex-wrap gap-1" />
+                {{-- <x-filament-notifications::actions :actions="$actions" class="flex-wrap gap-1" /> --}}
             </div>
-        @elseif (! $title && $body && $actions)
+        @elseif (!$title && $body && $actions)
             <div @class([
                 'flex flex-row flex-wrap items-center gap-4 leading-none',
                 match ($bodyAlignment) {
@@ -65,7 +67,7 @@
             ])>
                 <span class="text-sm">{{ $body }}</span>
 
-                <x-filament-notifications::actions :actions="$actions" class="flex-wrap gap-1" />
+                {{-- <x-filament-notifications::actions :actions="$actions" class="flex-wrap gap-1" /> --}}
             </div>
         @else
             <div @class([
@@ -94,7 +96,7 @@
                 <span class="text-sm">{{ $body }}</span>
 
                 @if ($actions)
-                    <x-filament-notifications::actions :actions="$actions" class="flex-wrap gap-1" />
+                    {{-- <x-filament-notifications::actions :actions="$actions" class="flex-wrap gap-1" /> --}}
                 @endif
             </div>
         @endif
